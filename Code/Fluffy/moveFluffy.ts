6ee55418f963d80aebda6a4936cb70d3e21a9f51
Sam <56440038@users.noreply.github.com>
2021-06-23 20:40:34 +0200

    color check

--- Code/Fluffy/moveFluffy.ts
+++ Code/Fluffy/moveFluffy.ts
@@ -38,9 +38,62 @@ namespace FluffyPong {
             fluffyDirection = new Vector(x, y);
             fluffyDirection.getDifference(oldPosition, fluffyDirection);
 
-            movedFluffy.move(fluffyDirection);
-            movedFluffy.draw();
-            fluffyTroughWall();
+            let position: Vector = new Vector(movedFluffy.position.x, movedFluffy.position.y);
+            for (let element of walls) {
+                if (element.position.x + borderWidth > position.x - (fluffyWidth / 2) && movedFluffy.colorenum == element.colorenum) {
+                    movedFluffy.move(fluffyDirection);
+                    movedFluffy.draw();
+                    console.log("passed the left wall");
+                    sendFluffy(movedFluffy, "right");
+
+                    fluffies.splice(fluffies.indexOf(movedFluffy), 1);
+                    crc2.putImageData(imgData, 0, 0);
+                    for (let fluffy of fluffies) {
+                        fluffy.draw();
+                    }
+                } else if (element.position.y + borderWidth > position.y - (fluffyHeight / 2) && movedFluffy.colorenum == element.colorenum) {
+                    movedFluffy.move(fluffyDirection);
+                    movedFluffy.draw();
+                    console.log("passed the top wall");
+                    sendFluffy(movedFluffy, "bottom");
+
+                    fluffies.splice(fluffies.indexOf(movedFluffy), 1);
+                    crc2.putImageData(imgData, 0, 0);
+                    for (let fluffy of fluffies) {
+                        fluffy.draw();
+                    }
+                } else if (position.x < position.x + (fluffyWidth / 2) && movedFluffy.colorenum == element.colorenum) {
+                    movedFluffy.move(fluffyDirection);
+                    movedFluffy.draw();
+                    console.log("passed the right wall");
+                    sendFluffy(movedFluffy, "left");
+
+                    fluffies.splice(fluffies.indexOf(movedFluffy), 1);
+                    crc2.putImageData(imgData, 0, 0);
+                    for (let fluffy of fluffies) {
+                        fluffy.draw();
+                    }
+                } else if (position.y < position.y + (fluffyHeight / 2) && movedFluffy.colorenum == element.colorenum) {
+                    movedFluffy.move(fluffyDirection);
+                    movedFluffy.draw();
+                    console.log("passed the bottom wall");
+                    sendFluffy(movedFluffy, "top");
+
+                    fluffies.splice(fluffies.indexOf(movedFluffy), 1);
+                    crc2.putImageData(imgData, 0, 0);
+                    for (let fluffy of fluffies) {
+                        fluffy.draw();
+                    }
+                } else {
+                    movedFluffy.move(oldPosition);
+                    movedFluffy.draw();
+                    crc2.putImageData(imgData, 0, 0);
+                    for (let fluffy of fluffies) {
+                        fluffy.draw();
+                    }
+                }
+            }
+            //fluffyTroughWall();
         }
 
         // wenn fluffy durch mauer geht sendFlufrfy in client aufrufen, um ihn an server zu schicken       
@@ -50,7 +103,7 @@ namespace FluffyPong {
         swipe = false;
     }
 
-    export function fluffyTroughWall(): void {
+    /* export function fluffyTroughWall(): void {
         let position: Vector = new Vector(movedFluffy.position.x, movedFluffy.position.y);
 
         for (let element of walls) {
@@ -92,26 +145,26 @@ namespace FluffyPong {
                 }
             } else {
                 break;
-            }
-
-            /* if (position.x < (fluffyWidth / 2) && movedFluffy.colorenum == element.colorenum) {
-                console.log("passed the left wall");
-                sendFluffy(movedFluffy, "right");
-            } else if (position.y < (fluffyHeight / 2) && movedFluffy.colorenum == element.colorenum) {
-                console.log("passed the top wall");
-                sendFluffy(movedFluffy, "bottom");
-            } else if (position.x > crc2.canvas.width - (fluffyWidth / 2) && movedFluffy.colorenum == element.colorenum) {
-                console.log("passed the right wall");
-                sendFluffy(movedFluffy, "left");
-            } else if (position.y > crc2.canvas.height - (fluffyHeight / 2) && _movedFluffy.colorenum == element.colorenum) {
-                console.log("passed the bottom wall");
-                sendFluffy(movedFluffy, "top");
-            } else {
-                break;
             } */
-        }
-        //check if the fluffy passed through a wall
+
+    /* if (position.x < (fluffyWidth / 2) && movedFluffy.colorenum == element.colorenum) {
+        console.log("passed the left wall");
+        sendFluffy(movedFluffy, "right");
+    } else if (position.y < (fluffyHeight / 2) && movedFluffy.colorenum == element.colorenum) {
+        console.log("passed the top wall");
+        sendFluffy(movedFluffy, "bottom");
+    } else if (position.x > crc2.canvas.width - (fluffyWidth / 2) && movedFluffy.colorenum == element.colorenum) {
+        console.log("passed the right wall");
+        sendFluffy(movedFluffy, "left");
+    } else if (position.y > crc2.canvas.height - (fluffyHeight / 2) && _movedFluffy.colorenum == element.colorenum) {
+        console.log("passed the bottom wall");
+        sendFluffy(movedFluffy, "top");
+    } else {
+        break;
+    } */
+    //}
+    //check if the fluffy passed through a wall
 
 
-    }
+    //}
 } //namespace
